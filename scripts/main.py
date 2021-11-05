f46e89f598072873e3deb6e5ca213ff107dd6c5c
Gregor Williamson <75175920+gregorwilliamson@users.noreply.github.com>
2021-11-05 14:52:43 -0400

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -9,23 +9,29 @@ into nltk.sem.logic.Expression objects.
 """
 
 if __name__ == '__main__':
-
+    
     # reentrant node
-    ex1 = "(a / admire-01 :ARG0 (b / boy) :ARG1 b))"  
+    ex1 = "(a / admire-01 :ARG0 (b / boy) :ARG1 b))"
+
     # :content role
-    ex2 = "(w / want-01 :ARG0 (b / boy) :content (b2 / believe-01 :ARG0 (g / girl) :ARG1 b))"   
+    ex2 = "(w / want-01 :ARG0 (b / boy) :content (b2 / believe-01 :ARG0 (g / girl) :ARG1 b))"
+
     # scope node
-    ex3 = "(s / scope :ARG0 b :ARG1 b2 :pred (r / read-01 :ARG0 (b / boy :quant every) :ARG1 (b2 / book :quant some)))" 
+    ex3 = "(s / scope :ARG0 b :ARG1 b2 :pred (r / read-01 :ARG0 (b / boy :quant every) :ARG1 (b2 / book :quant some)))"
+
     # intermediate scope
     ex4 = "(t / think-01 :ARG0 (b / boy) :content (s / scope :ARG0 v :pred (h / hope-01 :ARG0 (g / girl)" \
-          " :content (b2 / buy-01 :ARG0 g :ARG1 (v / violin :quant a)))))"  
+          " :content (b2 / buy-01 :ARG0 g :ARG1 (v / violin :quant a)))))"
+
     # disjunction and conjunction
-    ex5 = "(o / or :op1 (r / rain) :op2 (a / and :op1 (s / sun) :op2 (s2 / snow)))"  
+    ex5 = "(o / or :op1 (r / rain) :op2 (a / and :op1 (s / sun) :op2 (s2 / snow)))"
+
     # wide scope negation
-    ex6 = "(s / scope :ARG0 n :ARG1 g :pred (a / arrive-01 :ARG0 (g / girl :quant every) :polarity (n / -)))"  
+    ex6 = "(s / scope :ARG0 n :ARG1 g :pred (a / arrive-01 :ARG0 (g / girl :quant every) :polarity (n / -)))"
+
     # higher-order quantifiers
     ex7 = "(s / scope :ARG0 t :ARG1 w :pred (t2 / touch :ARG0 (t / table :quant most) " \
-          ":ARG1 (w / wall :quant exactly-one)))"   
+          ":ARG1 (w / wall :quant exactly-one)))"  
 
     for ex in [ex1, ex2, ex3, ex4, ex5, ex6, ex7]:
         print(f'[[ {ex} ]]')
